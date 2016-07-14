6ff0a2269991a9861ee43e9972cd7b8823022c95
qingyuan <ae5656ln@163.com>
2016-07-15 00:07:35 +0800

    update

--- test/createkey.py
+++ test/createkey.py
@@ -2,13 +2,14 @@ __author__ = 'PC-LiNing'
 
 from bigchaindb import crypto
 
+import ed25519
+import base58
 
-testuser1_priv, testuser1_pub = crypto.generate_key_pair()
+private,public=crypto.generate_key_pair()
+print("public key is : "+public)
 
-print(testuser1_priv, testuser1_pub)
+#private.encode().decode('ascii').
 
-print('#####')
-
-testuser2_priv, testuser2_pub = crypto.generate_key_pair()
-
-print(testuser2_priv, testuser2_pub)
+# sk,vk=ed25519.create_keypair()
+# private_value_base58 = crypto.Ed25519SigningKey(base58.b58encode(sk.to_bytes())).to_ascii()
+# print("private key generate public key is : "+str(sk.get_verifying_key().to_ascii(encoding="hex")))
