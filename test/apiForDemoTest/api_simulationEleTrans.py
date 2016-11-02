978c8b05660affab48d4006150e94c9b0ca807e3
lizhen <294474470@qq.com>
2016-11-02 20:42:30 +0800

    #lizhen.电力交易模拟API。API endpoints for electric demo

--- test/apiForDemoTest/api_simulationEleTrans.py
+++ test/apiForDemoTest/api_simulationEleTrans.py
@@ -13,9 +13,8 @@ if not processCount:
     print("please input how many processes do you want to create!")
     sys.exit()
 
-
-url = 'http://127.0.0.1:9984/apifordemo/v1/electric_trans/'
-# url = 'http://36.110.115.195:38/apifordemo/v1/electric_trans/'
+# url = 'http://127.0.0.1:9984/apifordemo/v1/electric_trans/'
+url = 'http://36.110.115.195:38/apifordemo/v1/electric_trans/'
 headers = {'content-type': 'application/json'}
 count = 0
 # open file
@@ -25,11 +24,11 @@ keypairAll = json.load(f)
 # start = time.time()
 # while (True):
 # 此次执行创建多少交易
-random_transactions = random.Random().randint(1, 100)
+random_transactions = random.Random().randint(500, 999)
 # 创建交易后休息多少秒
-sleep_random = random.Random().randint(0, 3)
+sleep_random = random.Random().randint(2, 5)
 
-print('will create %d transactions and then will sleep %ds' % (random_transactions, sleep_random))
+
 
 # for j in range(10):
 #     id = random.Random().randint(0, 99)
@@ -52,9 +51,13 @@ print('will create %d transactions and then will sleep %ds' % (random_transactio
 # time.sleep(sleep_random)
 
 def startRun():
+    global sleep_random
+    global random_transactions
+    time.sleep(sleep_random)
     global count
     print("start Run...............")
-    for i in range(1000):
+    print('will create %d transactions and then will sleep %ds' % (random_transactions, sleep_random))
+    for i in range(random_transactions):
         id = random.Random().randint(0, 99)
         owner_before = keypairAll["keypair"][id]["public_key"]
         private_key = keypairAll["keypair"][id]["private_key"]
@@ -71,14 +74,15 @@ def startRun():
         data = json.dumps(payload)
         ret = requests.post(url, data=data, headers=headers)
         count = count + 1
-
+        random_transactions = random.Random().randint(500, 999)
+        sleep_random = random.Random().randint(1, 4)
     return None
 
 
 if __name__ == "__main__":
   pool = mp.Pool(processes=int(processCount))
   while True:
-    print(count)
+    # print(count)
     pool.apply_async(startRun)
   pool.close()
   pool.join()
