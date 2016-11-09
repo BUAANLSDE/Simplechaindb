7b6fa21c9161fbe8357e1ff8c51e4d79955ad032
lizhen <294474470@qq.com>
2016-11-09 15:00:55 +0800

    #lizhen.电力交易模拟API。API endpoints for electric demo

--- test/apiForDemoTest/api_simulationEleTrans.py
+++ test/apiForDemoTest/api_simulationEleTrans.py
@@ -8,6 +8,7 @@ import datetime
 import uuid
 
 processCount = sys.argv[1]
+
 if not processCount:
     print("please input how many processes do you want to create!")
     sys.exit()
@@ -29,7 +30,6 @@ headers = {'content-type': 'application/json'}
 # # 创建交易后休息多少秒
 # sleep_random = random.Random().randint(2, 5)
 
-
 # for j in range(10):
 #     id = random.Random().randint(0, 99)
 #     owner_before = keypairAll["keypair"][id]["public_key"]
@@ -47,23 +47,36 @@ headers = {'content-type': 'application/json'}
 #     data = jdson.dumps(payload)
 #     ret = requests.post(url, data=data, headers=headers)
 #     count = count + 1
-#
 # time.sleep(sleep_random)
 
 def startRun():
     while True:
+
         random_transactions = random.Random().randint(500, 999)
         sleep_random = random.Random().randint(1, 4)
         print('will create %d transactions and then sleep %ds' % (random_transactions, sleep_random))
+        starttimefor = datetime.datetime.now().microsecond
+        count = 0
         for i in range(random_transactions):
             payload = {
                 "private_key": "7JVZtx9tCQD12HXG2J1QntrhcntNu7Sp3BCPF3H7fUhY"
-
             }
             data = json.dumps(payload)
-            ret = requests.post(url, data=data, headers=headers)
-            print(ret)
+            # starttime = datetime.datetime.now().microsecond
+            try:
+                ret = requests.post(url, data=data, headers=headers)
+            except:
+                print("post except!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            # endtime = datetime.datetime.now().microsecond
+            # responseTime = float(ret.elapsed.microseconds) / 1000  # 获取响应时间，单位ms
+            count += 1
+            print('count = %d '%(count))
+            # print('%d sys %d,one trans cost %d'%(count,ret.status_code,responseTime))
+            # print('local %d,one trans cost %d' % (ret.status_code, (endtime-starttime)/1000))
+        endtimefor = datetime.datetime.now().microsecond
+        print('cost %d ms create %d transactions' % ((endtimefor-starttimefor)/1000,random_transactions))
         time.sleep(sleep_random)
+
 
 if __name__ == "__main__":
     pool = mp.Pool(processes=int(processCount))
